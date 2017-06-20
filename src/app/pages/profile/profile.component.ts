f69dfd0b99e66d733efde8293bf33cc34c9636fb
welcome <welcomeking@outlook.com>
2017-06-20 11:07:56 +0200

    Update
    Node.js as well as angular design

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profile',
+  template: `<strong>My page content here</strong>`,
+})
+export class ProfileComponent {
+  constructor() {}
+}
